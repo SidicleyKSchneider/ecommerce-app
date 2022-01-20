3e303e54e90dfe156bb3b716d281ca534e79c467
Sidicley Schneider <sidi.fev@gmail.com>
2022-01-19 22:07:34 -0300

    install redux-thunk

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
-import {createStore} from 'redux';
-import data from './data';
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import data from "./data";
 
 const initialState = {};
-const reducer = (state, action) =>{
-    return{product: data.products};
+const reducer = (state, action) => {
+  return { product: data.products };
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
